d98bde4d6ad9c0f90dadece3e26123b54e2a73dc
danya <danstolyarov79@gmail.com>
2023-12-13 13:10:49 +0300

    added simple authorization

--- intelligent-hostel/Program.cs
+++ intelligent-hostel/Program.cs
@@ -1,23 +1,81 @@
 using intelligent_hostel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Npgsql;
-
-var builder = WebApplication.CreateBuilder(
-    new WebApplicationOptions { WebRootPath = "build"});
-var app = builder.Build();
+using System.Security.Claims;
+using System;
 await Database.init();
 
-var connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=dormitory_db";
-await using var dataSource = NpgsqlDataSource.Create(connectionString);
-app.MapGet("/", async (HttpContext context) =>
-{
-    await using (var cmd = dataSource.CreateCommand("SELECT * FROM users"))
-    await using (var reader = await cmd.ExecuteReaderAsync())
-    {
-        while (await reader.ReadAsync())
-        {
-            await context.Response.WriteAsync(reader.GetString(0));
-        }
-    }
+var builder = WebApplication.CreateBuilder(/*
+    new WebApplicationOptions { WebRootPath = "build"}*/);
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/login", async (HttpContext context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    // html-форма для ввода логина/пароля
+    string loginForm = @"<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='utf-8' />
+        <title>METANIT.COM</title>
+    </head>
+    <body>
+        <h2>Login Form</h2>
+        <form method='post'>
+            <p>
+                <label>Email</label><br />
+                <input name='email' />
+            </p>
+            <p>
+                <label>Password</label><br />
+                <input type='password' name='password' />
+            </p>
+            <input type='submit' value='Login' />
+        </form>
+    </body>
+    </html>";
+    await context.Response.WriteAsync(loginForm);
+});
+app.MapGet("/getinfo", [Authorize] async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("you`re authorized!");    
+});
+
+app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+{
+    // получаем из формы email и пароль
+    var form = context.Request.Form;
+    // если email и/или пароль не установлены, посылаем статусный код ошибки 400
+    if (!form.ContainsKey("email") || !form.ContainsKey("password"))
+        return Results.BadRequest("Email и/или пароль не установлены");
+
+    string email = form["email"];
+    string password = form["password"];
+    if (String.IsNullOrWhiteSpace(email) || String.IsNullOrWhiteSpace(password))
+        return Results.BadRequest("Email и/или пароль некорректны");
+
+    if (email != "test@example.lol" || password != "12345678") return Results.Unauthorized();
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, email) };
+    // создаем объект ClaimsIdentity
+    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    // установка аутентификационных куки
+    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+    return Results.Redirect(returnUrl ?? "/getinfo");
+});
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/login");
 });
 app.Run();
